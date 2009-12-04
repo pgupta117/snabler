3a7db92699d4742c21d8457e20279dc954153eb0
amund <amund@atbrox.com>
2009-12-04 02:48:57 -0800

    added variable increment size for mapper and added a short README

--- psvm/mapper.py
+++ psvm/mapper.py
@@ -9,12 +9,23 @@ import numpy
 import mrtools
 
 def map(key, value):
-    key = [float(i) for i in key.split(",")]
-    value = [float(i) for i in value.split(",")]
-    D = numpy.diag(key)
-    A = numpy.matrix(value)
+    trainingclasses = [float(i) for i in key.split(",")]
+    trainingfeatures = [float(i) for i in value.split(",")]
+
+    # if more than one class in classes, the features contains
+    # |classes| consecutive training example feature vectors
+    # so need to reshape it, to have 1 per row
+    numtrainingclasses = len(trainingclasses)
+    numtrainingfeatures = len(trainingfeatures)
+    numfeaturesperexample = numtrainingfeatures/numtrainingclasses
+
+    A = numpy.matrix(
+        numpy.reshape(numpy.array(trainingfeatures), 
+                      (numtrainingclasses,numfeaturesperexample)))
+    D = numpy.diag(trainingclasses)
     e = numpy.matrix(numpy.ones(len(A)).reshape(len(A),1))
     E = numpy.matrix(numpy.append(A,-e,axis=1))
+
     value = base64.b64encode(pickle.dumps((E.T*E, E.T*D*e)))
     print "outputkey\t%s" % ( value )
 
